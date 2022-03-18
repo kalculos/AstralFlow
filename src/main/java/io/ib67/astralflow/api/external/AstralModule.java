e4c006ea4c71e6f7b778c34218daec09f813fba9
iceBear67 <icebear67@sfclub.cc>
2022-03-18 21:57:43 +0800

    Add AstralModule and ModuleInfo

---
+++ src/main/java/io/ib67/astralflow/api/external/AstralModule.java
@@ -0,0 +1,46 @@
+/*
+ *
+ *   AstralFlow - The plugin who is turning bukkit into mod-pack
+ *   Copyright (C) 2022 iceBear67
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *   USA
+ */
+
+package io.ib67.astralflow.api.external;
+
+import org.jetbrains.annotations.ApiStatus;
+
+
+@ApiStatus.AvailableSince("0.1.0")
+public abstract class AstralModule {
+    private final ModuleInfo info;
+
+    public AstralModule(ModuleInfo info) {
+        this.info = info;
+    }
+
+    public void init() {
+
+    }
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+}
