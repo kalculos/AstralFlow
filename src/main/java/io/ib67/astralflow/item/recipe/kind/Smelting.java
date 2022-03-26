584f5b026270ebdb9ede8ab9d8ece8fb3b79beb2
iceBear67 <icebear67@sfclub.cc>
2022-03-26 23:47:52 +0800

    Add Smelting Recipe
    Removed unnecessary Shaped.PLACEHOLDER

---
+++ src/main/java/io/ib67/astralflow/item/recipe/kind/Smelting.java
@@ -0,0 +1,108 @@
+/*
+ *
+ *   AstralFlow - The plugin enriches bukkit servers
+ *   Copyright (C) 2022 The Inlined Lambdas and Contributors
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *   USA
+ */
+
+package io.ib67.astralflow.item.recipe.kind;
+
+import io.ib67.astralflow.item.recipe.AstralRecipe;
+import io.ib67.astralflow.item.recipe.IngredientChoice;
+import io.ib67.astralflow.item.recipe.RecipeType;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.function.Supplier;
+
+public class Smelting implements AstralRecipe {
+    private final NamespacedKey key;
+    private final IngredientChoice fuel;
+    private final IngredientChoice input;
+    private Supplier<ItemStack> result;
+    private ItemStack resultPrototype;
+
+    //todo: builder
+    private Smelting(NamespacedKey key, IngredientChoice fuel, IngredientChoice input, Supplier<ItemStack> result) {
+        this.key = key;
+        this.fuel = fuel;
+        this.input = input;
+    }
+
+    @Override
+    public RecipeType getRecipeType() {
+        return RecipeType.SMELTING;
+    }
+
+    @Override
+    public ItemStack produceResult() {
+        return result.get();
+    }
+
+    @Override
+    public ItemStack getPrototype() {
+        return resultPrototype;
+    }
+
+    @Override
+    public void setPrototype(ItemStack itemStack) {
+        this.resultPrototype = itemStack;
+    }
+
+    @Override
+    public IngredientChoice[] getMatrix() {
+        return new IngredientChoice[]{
+                input, fuel
+        };
+    }
+
+    @Override
+    public void setResult(Supplier<ItemStack> prototype) {
+        this.result = prototype;
+    }
+
+    @Override
+    public ItemStack[] apply(ItemStack[] itemStacks) {
+        if (!test(itemStacks)) {
+            throw new IllegalArgumentException("Invalid item stacks for this recipe.");
+        }
+        var input = itemStacks[0];
+        var fuel = itemStacks[1];
+        var result = Arrays.copyOf(itemStacks, itemStacks.length);
+        result[0] = this.input.apply(input);
+        result[1] = this.fuel.apply(fuel);
+        return result;
+    }
+
+    @Override
+    public boolean test(ItemStack[] itemStacks) {
+        if (itemStacks.length != 2) {
+            throw new IllegalArgumentException("Invalid matrix length for this smelting recipe. " + Arrays.toString(itemStacks));
+        }
+        var input = itemStacks[0];
+        var fuel = itemStacks[1];
+        return this.fuel.test(fuel) && this.input.test(input);
+    }
+
+    @NotNull
+    @Override
+    public NamespacedKey getKey() {
+        return key;
+    }
+}
