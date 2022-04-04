a15808fb68cfa0f1ca4660c5cac9d28cf8ff3587
iceBear67 <icebear67@sfclub.cc>
2022-04-04 18:02:13 +0800

    Add INSTANCE for WeaponCategory

--- src/main/java/io/ib67/astralflow/api/item/weapon/WeaponCategory.java
+++ src/main/java/io/ib67/astralflow/api/item/weapon/WeaponCategory.java
@@ -4,7 +4,9 @@ import io.ib67.astralflow.item.builder.ItemCategory;
 import io.ib67.astralflow.item.builder.ItemPrototype;
 import io.ib67.astralflow.item.factory.ItemPrototypeFactory;
 
-public class WeaponCategory implements ItemCategory<WeaponItem> {
+public final class WeaponCategory implements ItemCategory<WeaponItem> {
+    public static final WeaponCategory INSTANCE = new WeaponCategory();
+
     @Override
     public ItemPrototypeFactory getFactory(WeaponItem item) {
         return ItemPrototype.builder()
