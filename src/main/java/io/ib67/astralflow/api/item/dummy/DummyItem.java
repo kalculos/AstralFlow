6bb0a23e15e897faa3ac1e9a439ffa1505931c89
iceBear67 <icebear67@sfclub.cc>
2022-04-04 22:19:55 +0800

    Fix: DummyItem doesn't need an `@RequiredArgsConstructor`

--- src/main/java/io/ib67/astralflow/api/item/dummy/DummyItem.java
+++ src/main/java/io/ib67/astralflow/api/item/dummy/DummyItem.java
@@ -23,7 +23,6 @@ package io.ib67.astralflow.api.item.dummy;
 
 import io.ib67.astralflow.item.ItemKey;
 import io.ib67.astralflow.item.builder.ItemBuilder;
-import lombok.RequiredArgsConstructor;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
@@ -37,7 +36,6 @@ import java.util.Collections;
  * @param item prototype
  */
 @ApiStatus.AvailableSince("0.1.0")
-@RequiredArgsConstructor
 public record DummyItem(
         ItemKey key,
         ItemStack item,
