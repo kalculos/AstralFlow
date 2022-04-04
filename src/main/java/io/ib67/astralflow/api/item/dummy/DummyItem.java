7f6b7a6b4f0183bd2a3a96d3606d6cf50db5f7bc
iceBear67 <icebear67@sfclub.cc>
2022-04-04 21:37:05 +0800

    Add FetchMethod for DummyItem

--- src/main/java/io/ib67/astralflow/api/item/dummy/DummyItem.java
+++ src/main/java/io/ib67/astralflow/api/item/dummy/DummyItem.java
@@ -1,3 +1,24 @@
+/*
+ *
+ *   AstralFlow - The plugin enriches bukkit servers
+ *   Copyright (C) 2022 The Inlined Lambdas and Contributors
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *   USA
+ */
+
 package io.ib67.astralflow.api.item.dummy;
 
 import io.ib67.astralflow.item.ItemKey;
@@ -6,6 +27,8 @@ import lombok.RequiredArgsConstructor;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
 
 /**
  * Item without action.
@@ -17,15 +40,22 @@ import org.jetbrains.annotations.Nullable;
 @RequiredArgsConstructor
 public record DummyItem(
         ItemKey key,
-        ItemStack item
+        ItemStack item,
+        DummyItemProperty property
 ) {
+    public DummyItem {
+        for (FetchMethod fetchMethod : property.fetchMethods()) {
+            fetchMethod.init(() -> key.createNewItem().asItemStack());
+        }
+    }
+
     /**
      * Utility method to create a dummy item fastly.
      */
     public static ItemKey registerItem(ItemKey key, ItemStack item, @Nullable String oreDict) {
         ItemBuilder.of(DummyCategory.INSTANCE)
                 .oreDict(oreDict)
-                .prototype(new DummyItem(key, item))
+                .prototype(new DummyItem(key, item, new DummyItemProperty(Collections.emptyList())))
                 .register();
         return key;
     }
