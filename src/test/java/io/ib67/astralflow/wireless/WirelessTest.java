1e3dec48727783ddf4fa8be8bdce7ff666b4098f
iceBear67 <icebear67@sfclub.cc>
2022-05-03 17:30:13 +0800

    Add UnitTest for WirelessRegistry#findPeersAsync

--- src/test/java/io/ib67/astralflow/wireless/WirelessTest.java
+++ src/test/java/io/ib67/astralflow/wireless/WirelessTest.java
@@ -1,3 +1,24 @@
+/*
+ *
+ *   AstralFlow - The plugin enriches bukkit servers
+ *   Copyright (C) 2022 The Inlined Lambdas and Contributors
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *   USA
+ */
+
 package io.ib67.astralflow.wireless;
 
 import be.seeseemelk.mockbukkit.MockBukkit;
@@ -9,6 +30,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.util.concurrent.ExecutionException;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -19,6 +42,13 @@ public class WirelessTest {
     public void setup() {
         TestUtil.init();
         peerWorld = MockBukkit.getMock().addSimpleWorld("testPeers");
+    }
+
+    @Test
+    public void testPeerDiscoveryAsync() throws InterruptedException, ExecutionException {
+        SimplePeer.createBuilder()
+                .location(new Location(peerWorld, 3333, 0, 0)).build();
+        assertFalse(AstralFlow.getInstance().getWirelessRegistry().findPeersAsync(new Location(peerWorld, 3333, 2, 0), 3).get().isEmpty());
     }
 
     @Test
