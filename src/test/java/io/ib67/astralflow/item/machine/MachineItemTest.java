62d3dbb4c03b68537ee5576a7e09ead55b2f52c3
iceBear67 <icebear67@sfclub.cc>
2022-05-07 20:39:48 +0800

    Add MachineItemTest#testMachineBreak

--- src/test/java/io/ib67/astralflow/item/machine/MachineItemTest.java
+++ src/test/java/io/ib67/astralflow/item/machine/MachineItemTest.java
@@ -25,6 +25,7 @@ package io.ib67.astralflow.item.machine;
 import be.seeseemelk.mockbukkit.MockBukkit;
 import io.ib67.astralflow.api.AstralHelper;
 import io.ib67.astralflow.api.item.machine.MachineItem;
+import io.ib67.astralflow.hook.HookType;
 import io.ib67.astralflow.item.ItemKey;
 import io.ib67.astralflow.item.ItemKeys;
 import io.ib67.astralflow.test.TestUtil;
@@ -32,17 +33,25 @@ import io.ib67.astralflow.util.ItemStacks;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Item;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MachineItemTest {
     private static final ItemKey TEST_MACHINE_ITEM = ItemKeys.from("testitem:test_machine_item");
     private ItemKey machineItem;
     private World anyWorld;
+    private Block block;
 
     @BeforeAll
     public void setup() {
@@ -56,10 +65,11 @@ public class MachineItemTest {
     }
 
     @Test
+    @Order(1)
     public void testMachineSetup() {
-        var block = anyWorld.getBlockAt(0, 0, 0);
+        block = anyWorld.getBlockAt(0, 0, 0);
         var exampleItem = machineItem.createNewItem();
-        Assertions.assertTrue(AstralHelper.isItem(exampleItem.asItemStack()), "Test Item Allocation");
+        assertTrue(AstralHelper.isItem(exampleItem.asItemStack()), "Test Item Allocation");
         var player = MockBukkit.getMock().addPlayer();
         var blockPlaceEvent = new BlockPlaceEvent(
                 block,
@@ -71,6 +81,23 @@ public class MachineItemTest {
                 null
         );
         Bukkit.getPluginManager().callEvent(blockPlaceEvent);
-        Assertions.assertTrue(AstralHelper.hasMachine(block), "Test Block Placement");
+        assertTrue(AstralHelper.hasMachine(block), "Test Block Placement");
+    }
+
+    @Test
+    @Order(2)
+    public void testMachineBreak() {
+        AtomicReference<List<Item>> results = new AtomicReference<>();
+        HookType.BLOCK_DROP_ITEM.register(bdie -> {
+            results.set(bdie.getItems());
+        });
+        block.breakNaturally();
+        var bbe = new BlockBreakEvent(block, MockBukkit.getMock().addPlayer());
+        Bukkit.getPluginManager().callEvent(bbe);
+        assertTrue(results.get().size() > 0, "Size is bigger than zero");
+        var item = results.get().get(0).getItemStack();
+        assertTrue(AstralHelper.isItem(item), "The first one is a machine item");
+        assertFalse(AstralHelper.hasMachine(block), "Block is removed.");
+
     }
 }
