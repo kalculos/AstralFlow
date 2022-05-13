a9b3eb25432f49d1e5839deda8c07ea9b29ebc2e
iceBear67 <icebear67@sfclub.cc>
2022-05-13 17:03:43 +0800

    Fix: Ignored durability in TagChoice

--- src/main/java/io/ib67/astralflow/item/recipe/choices/TagChoice.java
+++ src/main/java/io/ib67/astralflow/item/recipe/choices/TagChoice.java
@@ -28,6 +28,7 @@ import lombok.Getter;
 import org.bukkit.Material;
 import org.bukkit.Tag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
 import org.jetbrains.annotations.ApiStatus;
 
 import java.util.List;
@@ -58,6 +59,19 @@ public class TagChoice implements IngredientChoice {
 
     @Override
     public boolean test(ItemStack itemStack) {
+        if (itemStack == null) {
+            return false;
+        }
+        if (getDurability() > 0) {
+            var meta = itemStack.getItemMeta();
+            if (meta instanceof Damageable damageable) {
+                if (damageable.getDamage() - getDurability() < 0) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
         return tag.isTagged(itemStack.getType());
     }
 
